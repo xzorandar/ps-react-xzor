43322adfd13ecaf96f22fcac744441195d96f966
xzorandar <brelandjohn@gmail.com>
2018-01-31 09:51:06 -0500

    Completed the documentation generator

---
+++ src/docs/CodeExample.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../prism';
+import {PrismCode} from "react-prism";
+import '../prism-okaida.css';
+
+
+class CodeExample extends React.Component {
+  render() {
+    return (
+      <pre ref={ref => { this.element = ref }}>
+          <PrismCode className="language-jsx">
+            <code>
+              {this.props.children}
+            </code>
+            </PrismCode>
+      </pre>
+    )
+  }
+}
+
+CodeExample.propTypes = {
+  children: PropTypes.string.isRequired
+}
+
+export default CodeExample; 
+// </PrismCode> <PrismCode className="language-jsx">
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Docs from './docs/Docs'
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Docs />, 
+  document.getElementById('root'));
